deb3643ee3436c0a2549ede834ec9e1dce4430fc
zhushizhang126 <zhushizhang126@qq.com>
2018-10-03 12:09:09 +0800

    init

---
+++ src/main/groovy/com/mmall/concurrency/ConcurrencyApplication.groovy
@@ -0,0 +1,12 @@
+package com.mmall.concurrency
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ConcurrencyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ConcurrencyApplication, args
+	}
+}
